735055c82cedc9a2a5a05a737f800b0d2c618b5c
Kevin Lam <klam@Kevins-Macbook-Pro.local>
2019-05-16 10:23:30 -0700

    Revises code to include API call

--- script.js
+++ script.js
@@ -16,11 +16,33 @@ function getUsers() {
 	.then(res => res.json())
 	.then(data => {
 		let output = '<h2>Users</h2>';
-		data.forEach((user) => {
+		data.forEach(user => {
 			output += `<p><strong>User: </strong> ${user.name}</p>`;
 			output += `<p><strong>Email:</strong>  <em>${user.email}</em></p><br />`;
 		});
 		document.getElementById('fetchData').innerHTML = output;
 	})
-	.catch(err => 'You can\'t sit with us!');
+	.catch(err => 'You can\'t sit with us!', err);
 };
+
+document.querySelector('#runAPI').addEventListener('click', getAPI);
+
+function getAPI() {
+	let gifs = '';
+	fetch(`http://api.giphy.com/v1/gifs/search?q=mean+girls&api_key=${config.apiKey}&limit=5`)
+	.then(res => res.json())
+	.then (data => {
+		data.data.map(gif => {
+			gifs += `<img src="${gif.images.fixed_width.url}" alt="${gif.title}" /><br />`
+		})
+		document.getElementById('fetchData').innerHTML = gifs;
+	})
+	.catch(err => console.log('She doesn\'t even go here!', err));
+}
+
+
+
+
+
+
+
