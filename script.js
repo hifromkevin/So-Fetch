6520334f30275324d8b60fe4fe3ce9b6aafcaeb0
Kevin Lam <klam@Kevins-Macbook-Pro.local>
2019-05-15 23:34:58 -0700

    Successfully displays gif and text

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 document.getElementById('getText').addEventListener('click', getText);
 
-// function getText() {
-// 	fetch('sample.txt')
-// 	.then(res => console.log(res))
-// 	.then(data => {
-// 		console.log(data);
-// 	})
-// 	.catch(err => 'You can\'t sit with us!');
-// };
-
 function getText() {
-	let xhr = new XMLHttpRequest();
+	fetch('sample.txt')
+	.then(res => res.text())
+	.then(data => {
+		document.getElementById('fetchData').innerHTML = data;
+	})
+	.catch(err => 'You can\'t sit with us!');
+};
 
-	xhr.open('GET', 'sample.txt');
-	xhr.onload = function() {
-		if (this.status === 200) {
-			console.log(this.responseText);
-		} else {
-			console.log('She doesn\'t even go here!');
-			
-		}
-	};
-	xhr.send();
-}
+// function getText() {
+// 	let xhr = new XMLHttpRequest();
+
+// 	xhr.open('GET', 'sample.txt');
+// 	xhr.onload = function() {
+// 		if (this.status === 200) {
+// 			console.log(this.responseText);
+// 		} else {
+// 			console.log('She doesn\'t even go here!');
+// 		}
+// 	};
+// 	xhr.send();
+// }
